3f428679c01af55cc4210e1ad4c2498f26771224
unknown <3337210017@untirta.ac.id>
2024-01-25 13:10:03 +0700

    add : style css

--- index.php
+++ index.php
@@ -36,36 +36,41 @@ if ($fileOpen) {
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 </head>
 
-<body>
-    <section class="max-w-screen-xl">
-        <h1>Advnace Financial System</h1>
-        <h2>Balance
+<body class="bg-gray-100 max-w-screen-sm mx-auto">
+    <section class="bg-gray-100">
+        <h1 class="pt-20 font-bold text-5xl">Advanced Financial System</h1>
+        <h2 class="font-semibold text-xl py-5">Balance
             <?php echo 'Rp ' . number_format((float) $balance, 2, ',', '.');  ?>
         </h2>
         <form action="process.php" method="post">
-            <div class=" ">
-                <p>Transaction Type : </p>
-                <select name="type" required>
-                    <option value="income">income</option>
-                    <option value="expense">expense</option>
-                </select>
+
+            <div class="bg-white rounded-xl shadow p-5">
+                <div class="flex items-center">
+                    <p class="">Transaction Type : </p>
+                    <select class="py-2 px-3 border-2 mx-5 rounded-md" name="type" required>
+                        <option value="income">income</option>
+                        <option value="expense">expense</option>
+                    </select>
+                </div>
+                <div class="flex items-center pt-5">
+                    <p>Amount : </p>
+                    <input class="py-2 px-3 border-2 ml-5 rounded-md" type="number" name="amount"></input>
+                </div>
+
             </div>
-            <div class="flex ">
-                <p>Amount : </p>
-                <input type="number" name="amount"></input>
-            </div>
-            <div>
-                <input type="submit"></input>
-            </div>
+            <input class="rounded my-5 py-3 px-4 bg-green-500 font-semibold text-white" type="submit"></input>
         </form>
-        <div>
+        <div class="py-10">
             <?php
             $fileRead = fopen('file.csv', 'r');
             if ($fileRead) {
-                echo '<table>';
-                echo '<tr>';
+                echo '<table class="w-full py-2 rounded-xl bg-white table-auto shadow">';
+                echo '<tr class="border-b-2 border-black">';
                 echo '<th>Type</th>';
                 echo '<th>Amount</th>';
                 echo '<th>Date</th>';
@@ -73,7 +78,7 @@ if ($fileOpen) {
 
                 while (($row = fgetcsv($fileRead)) !== false) {
                     if (is_array($row)) {
-                        echo '<tr>';
+                        echo '<tr class="py-2 px-3>';
 
                         foreach ($row as $index => $cell) {
                             if ($index === 1) {
